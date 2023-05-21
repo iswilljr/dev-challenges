e7689f422410d41f6012be5615b860a448cd9c3c
iswilljr <iswilljr@gmail.com>
2023-05-21 14:48:02 -0500

    add new info to categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className='space-y-8 py-8'>
       <section className='mx-auto max-w-7xl px-4'>
         <h2 className='text-xl font-semibold'>How does Dev Challenges Work?</h2>
-        <div className='mt-4 grid items-center gap-6 sm:grid-cols-2 lg:sm:grid-cols-3'>
+        <div className='mt-4 grid items-center gap-6 sm:grid-cols-2 lg:grid-cols-3'>
           {steps.map(step => (
             <Card key={step.title} className='bg-transparent' {...step} />
           ))}
@@ -15,9 +15,15 @@ export default function Home() {
       </section>
       <section className='mx-auto max-w-7xl px-4'>
         <h2 className='text-xl font-semibold'>Challenge Categories</h2>
-        <div className='mt-4 grid items-center gap-6 sm:grid-cols-2 lg:sm:grid-cols-3'>
-          {categories.map(step => (
-            <Card key={step.title} {...step} />
+        <div className='mt-4 grid items-center gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+          {categories.map(category => (
+            <Card
+              key={category.title}
+              href={category.href}
+              image={category.image}
+              title={category.title}
+              description={category.description}
+            />
           ))}
         </div>
       </section>
--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -1,46 +1,126 @@
+import { ChallengeType } from '@prisma/client'
 import type { CardProps } from '@/components/card/card'
 
-export const categories: CardProps[] = [
+export interface Category extends Omit<CardProps, 'bottomSection'> {
+  id: string
+  type: ChallengeType
+  interestingComments: string[]
+  guide: string[]
+}
+
+export const categories: Category[] = [
   {
+    id: 'frontend',
+    type: ChallengeType.frontend,
     title: 'Frontend Development',
     description:
       'Master HTML, CSS, and JavaScript to build dynamic and interactive user interfaces. Learn how to handle events, manipulate the DOM, and create engaging web experiences.',
     image: '/images/categories/frontend.png',
     href: '/category/frontend',
+    interestingComments: [
+      'Utilize proper HTML semantic tags for improved accessibility and SEO',
+      'Experiment with different CSS layout techniques',
+      'Explore modern JavaScript features and syntax',
+    ],
+    guide: [
+      'Write clean and modular HTML, CSS, and JavaScript code',
+      'Ensure cross-browser compatibility',
+      'Optimize website performance by minimizing HTTP requests and using efficient coding practices',
+    ],
   },
   {
+    id: 'responsive',
+    type: ChallengeType.responsive,
     title: 'Responsive Web Development',
     description:
       'Discover the techniques and best practices for creating responsive websites that adapt seamlessly to different screen sizes and devices.',
     image: '/images/categories/responsive.png',
     href: '/category/responsive',
+    interestingComments: [
+      'Understand responsive design principles',
+      'Implement media queries and flexible layouts',
+      'Test and optimize for different devices and screen sizes',
+    ],
+    guide: [
+      'Ensure the website layout adapts to different devices',
+      'Optimize images and other assets for different resolutions',
+      'Test the responsiveness on multiple browsers and devices',
+    ],
   },
   {
+    id: 'frameworks',
+    type: ChallengeType.frameworks,
     title: 'Web Application Frameworks',
     description:
       'Explore popular frontend frameworks like React, Angular, or Vue.js, and learn how to build scalable and efficient web applications using these frameworks.',
     image: '/images/categories/frameworks.png',
     href: '/category/frameworks',
+    interestingComments: [
+      'Learn a popular framework (e.g., React, Angular, or Vue.js)',
+      'Learn the core concepts and features of the framework',
+      'Build a small project using the framework',
+    ],
+    guide: [
+      'Utilize the chosen framework to develop the web application',
+      'Follow best practices and conventions of the framework',
+      'Handle data binding, component communication, and routing',
+    ],
   },
   {
+    id: 'backend',
+    type: ChallengeType.backend,
     title: 'Backend Development',
     description:
       'Dive into backend development using technologies like Node.js, Python, or Ruby. Learn how to build APIs, handle data, and implement server-side logic.',
     image: '/images/categories/backend.png',
     href: '/category/backend',
+    interestingComments: [
+      'Explore server-side programming',
+      'Learn about building APIs',
+      'Handle data efficiently on the backend',
+    ],
+    guide: [
+      'Use a backend framework of your choice',
+      'Ensure secure handling of user data',
+      'Implement robust error handling and validation',
+    ],
   },
   {
+    id: 'databases',
+    type: ChallengeType.databases,
     title: 'Database Integration',
     description:
       'Learn to integrate databases like MySQL, PostgreSQL, or MongoDB into your web applications. Practice data modeling, querying, and working with database management systems.',
     image: '/images/categories/databases.png',
     href: '/category/databases',
+    interestingComments: [
+      'Explore different database management systems',
+      'Practice creating efficient database schemas',
+      'Learn advanced querying techniques',
+    ],
+    guide: [
+      'Choose a database technology suitable for your project',
+      'Ensure data integrity and security',
+      'Optimize database queries for performance',
+    ],
   },
   {
+    id: 'fullstack',
+    type: ChallengeType.fullstack,
     title: 'Full-Stack Development',
     description:
       'Combine frontend and backend development skills to create complete web applications. Learn how to handle data flow, implement user authentication, and build seamless user experiences.',
     image: '/images/categories/fullstack.png',
     href: '/category/fullstack',
+    interestingComments: [
+      'Experience end-to-end web development',
+      'Integrate frontend and backend seamlessly',
+      'Implement authentication and authorization',
+    ],
+    guide: [
+      'Ensure proper communication between frontend and backend',
+      'Implement user authentication and authorization',
+      'Focus on delivering a seamless user experience',
+    ],
   },
 ]
