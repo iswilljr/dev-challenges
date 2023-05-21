b72f6a93ae26efe57c40432015cb1a3f225eb6a5
iswilljr <iswilljr@gmail.com>
2023-05-21 12:13:10 -0500

    create new user profile
    remove option session.strategy

--- src/utils/auth-options.ts
+++ src/utils/auth-options.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import type { NextAuthOptions } from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
+import GithubProvider, { type GithubProfile } from 'next-auth/providers/github'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { prisma } from '@/utils/prisma'
 
@@ -10,10 +10,29 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      profile(profile: GithubProfile, tokens) {
+        return {
+          id: profile.id.toString(),
+          bio: profile.bio,
+          name: profile.name ?? profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+          username: profile.login,
+          twitterUsername: profile.twitter_username,
+          website: profile.blog,
+        }
+      },
     }),
   ],
+  callbacks: {
+    session({ session, user }) {
+      return {
+        user,
+        expires: session.expires,
+      }
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
-  session: { strategy: 'jwt' },
   pages: {
     signIn: '/',
     error: '/',
