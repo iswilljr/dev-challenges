d0cd11a9d6315ffd9b25710fd3328f8d24cff03d
iswilljr <iswilljr@gmail.com>
2023-05-21 14:49:16 -0500

    add category page

---
+++ src/app/category/[categoryId]/page.tsx
@@ -0,0 +1,70 @@
+import { notFound } from 'next/navigation'
+import { Categories } from '@/components/categories/categories'
+import { authOptions } from '@/utils/auth-options'
+import { categories } from '@/utils/categories'
+import { prisma } from '@/utils/prisma'
+import { getServerSession } from 'next-auth'
+import type { ChallengeType } from '@prisma/client'
+
+async function getCategoryChallenges(type: ChallengeType) {
+  try {
+    const session = await getServerSession(authOptions)
+
+    if (!session?.user?.email) return null
+
+    const challenges = await prisma.challenge.findMany({
+      where: {
+        userId: session.user.id,
+        type,
+      },
+    })
+
+    return challenges
+  } catch (error) {
+    return null
+  }
+}
+
+export default async function Category({ params }: { params: { categoryId: string } }) {
+  const category = categories.find(category => category.id === params.categoryId)
+
+  if (!category) notFound()
+
+  const challenges = await getCategoryChallenges(category.type)
+
+  return (
+    <div className='space-y-8 py-8'>
+      <section className='mx-auto max-w-7xl px-4'>
+        <div className='rounded-md border border-gray-500 p-4'>
+          <h2 className='text-xl font-semibold'>{category.title}</h2>
+          <p className='mt-2'>{category.description}</p>
+          <div className='space-y-4 py-4'>
+            <article className='rounded-md border border-blue-700 bg-blue-700/5 p-4'>
+              <h3 className='text-md font-medium'>Get into this category if you want to:</h3>
+              <ul className='mt-2'>
+                {category.interestingComments.map(comment => (
+                  <li key={comment} className='list-inside list-disc'>
+                    <p className='inline-block'>{comment}</p>
+                  </li>
+                ))}
+              </ul>
+            </article>
+            <article className='rounded-md border border-green-700 bg-green-700/5 p-4'>
+              <h3 className='text-md font-medium'>Guide:</h3>
+              <ul className='mt-2'>
+                {category.guide.map(comment => (
+                  <li key={comment} className='list-inside list-disc'>
+                    <p className='inline'>{comment}</p>
+                  </li>
+                ))}
+              </ul>
+            </article>
+          </div>
+        </div>
+      </section>
+      <section className='mx-auto max-w-7xl px-4'>
+        <Categories challenges={challenges} />
+      </section>
+    </div>
+  )
+}
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -5,10 +5,14 @@ import { Skeleton } from '../skeleton/skeleton'
 
 export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   bottomSection?: React.ReactNode
-  description?: string
+  description: string
   href?: string
-  image?: string
-  title?: string
+  image: string
+  title: string
+}
+
+export interface CardSkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+  animate?: boolean
 }
 
 export function Card({ description, href, image, title, className, ...props }: CardProps) {
@@ -25,32 +29,40 @@ export function Card({ description, href, image, title, className, ...props }: C
         href={href}
         {...props}
       >
-        {image && title ? (
-          <div className='relative h-44 w-full overflow-hidden rounded-md bg-white'>
-            <Image
-              className={cx('mx-auto object-contain', { 'duration-300 group-hover:scale-110': href })}
-              sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-              src={image}
-              alt={title}
-              fill
-            />
-          </div>
-        ) : (
-          <Skeleton className='h-44' />
-        )}
-        {title ? <h3 className='mt-4 text-lg font-semibold text-gray-200'>{title}</h3> : <Skeleton className='mt-4' />}
-        <div className='text-md mb-4 mt-2 text-gray-300/90'>
-          {description ?? (
-            <>
-              <Skeleton className='mt-1 h-2' />
-              <Skeleton className='mt-1 h-2' />
-              <Skeleton className='mt-1 h-2' />
-              <Skeleton className='mt-1 h-2' />
-              <Skeleton className='mt-1 h-2' />
-            </>
-          )}
+        <div className='relative h-44 w-full overflow-hidden rounded-md bg-white'>
+          <Image
+            className={cx('mx-auto object-contain', { 'duration-300 group-hover:scale-110': href })}
+            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+            src={image}
+            alt={title}
+            fill
+          />
         </div>
+        <h3 className='mt-4 text-lg font-semibold text-gray-200'>{title}</h3>
+        <div className='text-md mb-4 mt-2 text-gray-300/90'>{description}</div>
       </Component>
     </article>
+  )
+}
+
+export function CardSkeleton({ animate, ...props }: CardSkeletonProps) {
+  const animateClass = animate && 'animate-pulse'
+
+  return (
+    <div
+      {...props}
+      role='status'
+      className={cx('group block h-full rounded-md border border-gray-500 bg-zinc-800/50 p-4', props.className)}
+    >
+      <Skeleton className={cx('h-44', animateClass)} />
+      <Skeleton className={cx('mt-4', animateClass)} />
+      <div className='text-md mt-2 text-gray-300/90'>
+        <Skeleton className={cx('mt-1 h-2', animateClass)} />
+        <Skeleton className={cx('mt-1 h-2', animateClass)} />
+        <Skeleton className={cx('mt-1 h-2', animateClass)} />
+        <Skeleton className={cx('mt-1 h-2', animateClass)} />
+        <Skeleton className={cx('mt-1 h-2', animateClass)} />
+      </div>
+    </div>
   )
 }
