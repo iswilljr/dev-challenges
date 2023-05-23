103ef0e02df039b44fbf7cfd9b56e18894667846
iswilljr <iswilljr@gmail.com>
2023-05-23 18:15:12 -0500

    delete unnecessary files

--- src/app/challenge/[challengeId]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { categories } from '@/utils/categories'
-
-export async function generateStaticParams() {
-  return categories.map(category => ({ challengeId: category.id }))
-}
-
-export default function ChallengeLayout({ children }: React.PropsWithChildren) {
-  return children
-}
