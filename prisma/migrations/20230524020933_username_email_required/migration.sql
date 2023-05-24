572ef2ac9b56e570919bef37d493516eff64da15
iswilljr <iswilljr@gmail.com>
2023-05-24 11:30:58 -0500

    make username and email required

---
+++ prisma/migrations/20230524020933_username_email_required/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `username` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "email" SET NOT NULL,
+ALTER COLUMN "username" SET NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
