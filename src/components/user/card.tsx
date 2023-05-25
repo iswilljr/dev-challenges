d08094c0029bf0e66786a4b5fd63bf4bde15bc1c
iswilljr <iswilljr@gmail.com>
2023-05-25 17:25:41 -0500

    add website info to user card

--- src/components/user/card.tsx
+++ src/components/user/card.tsx
@@ -1,16 +1,34 @@
 import Link from 'next/link'
-import { BsTwitter } from 'react-icons/bs'
-import { TbMail } from 'react-icons/tb'
+import { BsGlobeAmericas, BsTwitter, BsEnvelopeAt } from 'react-icons/bs'
 import { Avatar } from '../avatar/avatar'
 import { Button } from '../button/button'
 import type { User } from '@prisma/client'
+import type { IconType } from 'react-icons'
 
 interface UserCardProps {
   user: User
   editProfileButton?: boolean
 }
 
+interface Item {
+  href: string
+  label?: string
+  icon: IconType
+}
+
 export function UserCard({ user, editProfileButton }: UserCardProps) {
+  const items = [
+    {
+      href: `mailto:${user.email}`,
+      label: user.email,
+      icon: BsEnvelopeAt,
+    },
+    user.website ? { href: user.website, icon: BsGlobeAmericas } : null,
+    user.twitterUsername
+      ? { href: `https://twitter.com/${user.twitterUsername}`, label: `@${user.twitterUsername}`, icon: BsTwitter }
+      : null,
+  ].filter((item): item is Item => item != null)
+
   return (
     <div className='space-y-3'>
       <Avatar
@@ -40,29 +58,19 @@ export function UserCard({ user, editProfileButton }: UserCardProps) {
       )}
 
       <ul className='text-gray-300'>
-        <li>
-          <a
-            className='flex items-center gap-2 duration-150 hover:text-white hover:underline'
-            href='mailto:iswilljr@gmail.com'
-          >
-            <TbMail className='text-gray-400' />
-            <span>{user.email}</span>
-          </a>
-        </li>
-
-        {user.twitterUsername && (
-          <li>
+        {items.map(item => (
+          <li key={item.href}>
             <a
               className='flex items-center gap-2 duration-150 hover:text-white hover:underline'
-              href={`https://twitter.com/${user.twitterUsername}`}
+              href={item.href}
               target='_blank'
               rel='noopener noreferrer'
             >
-              <BsTwitter className='text-gray-400' />
-              <span>{`@${user.twitterUsername}`}</span>
+              <item.icon className='h-5 w-5 flex-shrink-0 text-gray-400' />
+              <span className='truncate'>{item.label ?? item.href}</span>
             </a>
           </li>
-        )}
+        ))}
       </ul>
     </div>
   )
