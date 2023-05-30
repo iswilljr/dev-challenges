b9bf1ec7cde8e1b763098bcc827845467da3f286
iswilljr <iswilljr@gmail.com>
2023-05-30 10:56:33 -0500

    add create challenge link to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { Card } from '@/components/card/card'
 import { categories } from '@/utils/categories'
 import { steps } from '@/utils/steps'
@@ -14,7 +15,12 @@ export default function Home() {
         </div>
       </section>
       <section>
-        <h2 className='text-xl font-semibold'>Challenge Categories</h2>
+        <div className='flex items-center justify-between gap-2'>
+          <h2 className='text-xl font-semibold'>Challenge Categories</h2>
+          <Link className='text-end text-blue-300' href='/challenge/create'>
+            Create a custom challenge<span className='hidden md:inline'> with an InfoJobs Offer</span>
+          </Link>
+        </div>
         <div className='mt-4 grid items-center gap-6 sm:grid-cols-2 lg:grid-cols-3'>
           {categories.map(category => (
             <Card
